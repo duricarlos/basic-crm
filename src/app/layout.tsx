3ace1b70af8b6e62905644d5eb08ca2911b1fd7c
Carlos Duri <cduri.business@gmail.com>
2026-01-30 01:11:11 -0500

    refactor: mejorar la consistencia del formato en layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,30 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Basic CRM | Gestión Simplificada",
-  description: "Un CRM diseñado para ser extremadamente sencillo, intuitivo y fácil de usar. Gestiona tus clientes y presupuestos sin complicaciones.",
-};
+  title: 'Basic CRM | Gestión Simplificada',
+  description: 'Un CRM diseñado para ser extremadamente sencillo, intuitivo y fácil de usar. Gestiona tus clientes y presupuestos sin complicaciones.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="es">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='es'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
